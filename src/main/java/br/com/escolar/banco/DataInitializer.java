da54f8615bcddfe0de80472944e14cce57217d00
Gilnei aquino <gilnei.aquino@gmail.com>
2023-09-08 18:56:23 -0300

    CrAjuste testes

--- src/main/java/br/com/escolar/banco/DataInitializer.java
+++ src/main/java/br/com/escolar/banco/DataInitializer.java
@@ -30,19 +30,19 @@ public class DataInitializer implements CommandLineRunner {
         Telefone telefoneAluno1 = new Telefone("1","14","33147897", "Celular");
 
 
-        Aluno aluno1 = new Aluno("1L","Marcos", LocalDate.of(1998,05,11), "Masculino", Arrays.asList(enderecoAluno1), Arrays.asList(telefoneAluno1),"teste@teste1.com","01145461148","123456");
+        Aluno aluno1 = new Aluno("1","Marcos", LocalDate.of(1998,05,11), "Masculino", Arrays.asList(enderecoAluno1), Arrays.asList(telefoneAluno1),"teste@teste1.com","01145461148","123456");
 
         Endereco enderecoAluno2 = new Endereco("2L","25","Rua B", "Cidade B", "Estado B", "54321-876");
         Telefone telefoneAluno2 = new Telefone("2L","11","42553218", "Fixo");
 
 
-        Aluno aluno2 = new Aluno("2L","Maria", LocalDate.of(1998,01,15), "Feminino",Arrays.asList( enderecoAluno2), Arrays.asList(telefoneAluno2),"teste@teste2.com","01145461448","123457");
+        Aluno aluno2 = new Aluno("2","Maria", LocalDate.of(1998,01,15), "Feminino",Arrays.asList( enderecoAluno2), Arrays.asList(telefoneAluno2),"teste@teste2.com","01145461448","123457");
 
         Endereco enderecoAluno3 = new Endereco("3L","Rua C","36", "Cidade C", "Estado C", "98765-432");
         Telefone telefoneAluno3 = new Telefone("3L","61","32553255", "Celular");
 
 
-        Aluno aluno3 = new Aluno("3L","Pedro", LocalDate.of(2024,05,28), "Masculino",Arrays.asList( enderecoAluno3), Arrays.asList(telefoneAluno3),"teste@teste3.com","01145461177", "6798797987");
+        Aluno aluno3 = new Aluno("3","Pedro", LocalDate.of(2024,05,28), "Masculino",Arrays.asList( enderecoAluno3), Arrays.asList(telefoneAluno3),"teste@teste3.com","01145461177", "6798797987");
 
         // Salve os alunos no banco de dados
        alunoRepository.save(aluno1);
